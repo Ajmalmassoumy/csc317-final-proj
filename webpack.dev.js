bc01994580c24ac1390f061238f14f3d21ec7740
Michal Krupa <michalkrupa@gmail.com>
2025-05-20 13:55:39 -0700

    Shorten file name

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const common = require("./webpack.common");
+const common = require("./webpack");
 const path = require("path");
 
 module.exports = merge(common({ production: false }), {
